f6332956d8adaa040a6f572ffb088908c825d3e1
Freed <86789401+SKFreed@users.noreply.github.com>
2023-06-27 10:15:14 +0500

    path application.properties

--- src/main/java/com/example/telegrambotjava/config/BotConfig.java
+++ src/main/java/com/example/telegrambotjava/config/BotConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 @Data
 @Configuration
-@PropertySource("application.properties")
+@PropertySource("../../../../../resources/application.properties")
 public class BotConfig {
     @Value("${bot.name}")
     String botName;
