6db1f3c66a22b6b101a8c9a0caf46e57b17cf24e
Freed <86789401+SKFreed@users.noreply.github.com>
2023-06-27 10:05:26 +0500

    First

---
+++ src/test/java/com/example/telegrambotjava/TelegramBotJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telegrambotjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
